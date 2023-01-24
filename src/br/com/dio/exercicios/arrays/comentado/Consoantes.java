3017b0e58f50fd6142cdb26cecf99b10dc9904e1
Thiago-Hassan <thiagovenonsec@gmail.com>
2023-01-23 21:41:10 -0300

    Exercicio Nome E idade Redigitei o codigo, apos ter dado fork.

--- src/br/com/dio/exercicios/arrays/comentado/Consoantes.java
+++ src/br/com/dio/exercicios/arrays/comentado/Consoantes.java
@@ -1,10 +1,12 @@
 package br.com.dio.exercicios.arrays.comentado;
+
+import java.util.Scanner;
 /*
 Faça um Programa que leia um vetor de 6 caracteres,
 e diga quantas consoantes foram lidas.
 Imprima as consoantes.
 */
-import java.util.Scanner;
+
 
 public class Consoantes {
     public static void main(String[] args) {
